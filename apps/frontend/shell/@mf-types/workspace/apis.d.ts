5eac418434e05c10e19ff8e7cb65d85391a2c8d4
1114nikhil <nikhiltk1114@gmail.com>
2026-09-09 07:06:14 +0530

    changes after run

---
+++ apps/frontend/shell/@mf-types/workspace/apis.d.ts
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <script type="module">import { injectIntoGlobalHook } from "/@react-refresh";
+injectIntoGlobalHook(window);
+window.$RefreshReg$ = () => {};
+window.$RefreshSig$ = () => (type) => type;</script>
+
+    <script type="module" src="/@vite/client"></script>
+
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>workspace</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/@id/__x00__virtual:mf-html-entry-proxy?init=%2F%40id%2Fvirtual%3Amf%3A__mfe_internal__workspace__mf_owner__1__H_A_I__hostAutoInit__H_A_I__.js&entry=%2Fsrc%2Fmain.tsx"></script>
+  </body>
+</html>
